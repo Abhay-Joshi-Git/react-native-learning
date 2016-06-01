1579e8b829a3512628e323dbe7f14cc9dab786a2
abhay-joshi <abhay.joshi@drillinginfo.com>
2016-06-01 13:07:55 +0530

    added touchable highlight to list view

--- employeeList/src/components/departmentList.js
+++ employeeList/src/components/departmentList.js
@@ -3,7 +3,9 @@ import {
     View,
     Text,
     StyleSheet,
-    ListView
+    ListView,
+    TouchableHighlight,
+    TouchableNativeFeedback
 } from 'react-native';
 
 export default class DepartmentList extends React.Component {
@@ -25,20 +27,60 @@ export default class DepartmentList extends React.Component {
                 </View>
                 <ListView
                     dataSource={this.state.dataSource}
-                    renderRow={item => {
-                        return (
-                            <View style={styles.itemContainer}>
-                                <Text>Name:  {item.name}</Text>
-                                <Text>HOD: {item.hod} </Text>
-                            </View>
-                        )
-                    }}
-                ></ListView>
-
+                    renderRow={this.renderDepartmentRow.bind(this)}
+                >
+                </ListView>
             </View>
         )
     }
+
+    renderDepartmentRow(item) {
+        return (
+            <TouchableHighlight
+                underlayColor='rgb(0, 100, 150)'
+                onPress={() => {this.onDepartmentPress(item)} }
+            >
+                <View style={styles.itemContainer}>
+                    <View style={styles.labelRow}>
+                        <Text style={[styles.label, styles.textFont]}>Name:</Text>
+                        <Text style={styles.textFont}>{item.name}</Text>
+                    </View>
+                    <View style={styles.labelRow}>
+                        <Text style={styles.label}>HOD:</Text>
+                        <Text>{item.hod}</Text>
+                    </View>
+                </View>
+            </TouchableHighlight>
+        )
+    }
+
+    renderDepartmentRowWithTouchableNativeFeedback(item) {
+        return (
+            <TouchableNativeFeedback
+                onPress={this.onDepartmentPress.bind(this)}
+                background={TouchableNativeFeedback.Ripple('gray')}
+            >
+                <View style={styles.itemContainer}>
+                    <View style={styles.labelRow}>
+                        <Text style={[styles.label, styles.textFont]}>Name:</Text>
+                        <Text style={styles.textFont}>{item.name}</Text>
+                    </View>
+                    <View style={styles.labelRow}>
+                        <Text style={styles.label}>HOD:</Text>
+                        <Text>{item.hod}</Text>
+                    </View>
+                </View>
+            </TouchableNativeFeedback>
+        )
+    }
+
+
+    onDepartmentPress(depatment) {
+        console.log('on Department press', depatment);
+    }
 }
+
+
 
 var styles = StyleSheet.create({
     mainContainer: {
@@ -51,9 +93,19 @@ var styles = StyleSheet.create({
         alignItems: 'center',
         borderWidth: 2,
         borderRadius: 5,
-        borderColor: 'rgb(0, 200, 200)'
+        borderColor: 'rgb(0, 200, 200)',
+        backgroundColor: 'white'
     },
     header: {
         fontSize: 30
+    },
+    labelRow: {
+        flexDirection: 'row'
+    },
+    label: {
+        fontWeight: 'bold'
+    },
+    textFont: {
+        fontSize: 18
     }
 })
