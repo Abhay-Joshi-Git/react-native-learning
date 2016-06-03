5aaaf63e6a7fe80974798c6ded3a7777703450f4
abhay-joshi <abhay.joshi@drillinginfo.com>
2016-06-03 14:34:27 +0530

    added employees to get scrolling on

--- employeeList/mockData/employeeList.js
+++ employeeList/mockData/employeeList.js
@@ -14,5 +14,41 @@ export default [
     {
         name: 'Tango',
         department: 'sales'
+    },
+    {
+        name: 'Beta',
+        department: 'manufacturing'
+    },
+    {
+        name: 'Gamma',
+        department: 'manufacturing'
+    },
+    {
+        name: 'Epsilon',
+        department: 'manufacturing'
+    },
+    {
+        name: 'Thita',
+        department: 'manufacturing'
+    },
+    {
+        name: 'Lambda',
+        department: 'manufacturing'
+    },
+    {
+        name: 'Sigma',
+        department: 'manufacturing'
+    },
+    {
+        name: 'Omega',
+        department: 'manufacturing'
+    },
+    {
+        name: 'Ted',
+        department: 'manufacturing'
+    },
+    {
+        name: 'Zeta',
+        department: 'manufacturing'
     }
 ]
